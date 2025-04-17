c0ecaf8360fb63e64dddc1fed7864b701e350b46
Jeff Ruoss <jeffreyruoss@gmail.com>
2025-04-17 12:50:53 -0700

    Update backup icon

--- src/App.vue
+++ src/App.vue
@@ -151,10 +151,26 @@ onBeforeUnmount(() => {
               class="opacity-70 hover:opacity-100 text-sm uppercase px-3 py-1 rounded-md text-white border-[1px] border-gray-700 hover:cursor-pointer"
               title="Create and download backup of data"
             >
-              💾 Backup
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="inline-block mr-1"
+              >
+                <path d="M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v11a2 2 0 0 1-2 2z" />
+                <polyline points="17 21 17 13 7 13 7 21" />
+                <polyline points="7 3 7 8 15 8" />
+              </svg>
+              Backup
             </button>
             <div
-              class="absolute bottom-full mb-2 left-0 opacity-0 group-hover:opacity-100 transition-opacity bg-gray-800 text-white text-xs px-2 py-1 rounded whitespace-nowrap"
+              class="absolute top-full mt-2 left-0 opacity-0 group-hover:opacity-100 transition-opacity bg-gray-800 text-white text-xs px-2 py-1 rounded whitespace-nowrap"
             >
               Next auto-backup: {{ nextBackupTime }}
             </div>
